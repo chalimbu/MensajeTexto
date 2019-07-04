7ecfcdef8850dc4eb77e2f21765029fe426bc257
chalimbu <sebitaszapata12@gmail.com>
2019-07-04 14:19:01 -0500

    cabeceras CORS

--- src/index.js
+++ src/index.js
@@ -26,6 +26,13 @@ hbs.registerPartials(partialsPath)
 //setub static directory to server
 app.use(express.static(publicDirectoryPath))
 app.use(express.json())
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
 
 app.use(clienteRouter)
 app.use(userRouter)
